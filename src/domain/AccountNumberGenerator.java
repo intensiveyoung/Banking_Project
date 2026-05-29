ce663bd4ce8644f6399a86c07291e2f4e0492aad
intensiveyoung <jamesmwangi056@gmail.com>
2026-05-29 23:34:28 +0300

    feature(BANK-9): implement Sequential Account Number Generator.
    - Used AtomicInteger to guarantee unique account numbers even in the case of accounts being opened in the same exact millisecond.
    - Created a test testUniqueAccountNumbers to verify uniqueness and that account numbers start at 1001

--- src/domain/AccountNumberGenerator.java
+++ src/domain/AccountNumberGenerator.java
@@ -3,8 +3,13 @@ package domain;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class AccountNumberGenerator {
-    // Starts at 10001
-    private static final AtomicInteger counter = new AtomicInteger(10000);
+    // Starts at 1000 so the first incremented account is 1001
+    private static final AtomicInteger counter = new AtomicInteger(1000);
+
+    /**
+     * Increments the thread-safe counter and returns the next available account number.
+     * @return A unique sequential String identifier.
+     */
     public static String getNextAccountNumber() {
         return String.valueOf(counter.incrementAndGet());
     }
